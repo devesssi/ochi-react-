e4edf9cd0a6b040789612272c2dd57b4c670ad33
DINESH VERMA <dineshkumarverma.dev@gmail.com>
2024-10-05 16:39:50 +0530

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{jsx,css}",
+    './src/**/*.{js,jsx,ts,tsx}', 
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
